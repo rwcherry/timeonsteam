2616d4a51e5430d09d34167a9f19d4791155bd35
Rob Cherry <rakirte@gmail.com>
2021-03-13 20:45:11 -0800

    Theme is janky but it all works for now

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
-import './App.css';
-import { createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { CssBaseline, Grid } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/core/styles';
 import SearchField from './SearchField.js';
+import theme from './theme.js'
 
 function App() {
 
-  const darkTheme = createMuiTheme({
-    palette: {
-      type: 'dark',
-    },
-  });
-
   return (
-    <ThemeProvider theme={darkTheme}>
-    <div className="App">
-      <header className="App-header">
-        <h1>Time on Steam</h1> 
-        <SearchField />
-      </header>
-    </div>
-    </ThemeProvider>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Grid
+        container
+        spacing={0}
+        direction="column"
+        alignItems="center"
+        justify="center"
+        style={{ minHeight: '100vh' }}
+      >
+        <Grid item xs={12}>
+          <h1>Time on Steam</h1>
+          <SearchField />
+        </Grid>   
+      </Grid> 
+    </ThemeProvider >
   );
 }
 
--- src/SearchField.js
+++ src/SearchField.js
@@ -1,7 +1,26 @@
-import { makeStyles, createMuiTheme, ThemeProvider } from '@material-ui/core/styles';
+import { withStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableContainer from '@material-ui/core/TableContainer';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
 import React from 'react';
 
+import comparisonThings from "./data/time_comparisons.json"
+
+const useStyles = (theme) => ({
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    minWidth: 650,
+  },
+  table: {
+    minWidth: 650,
+  },
+});
 
 class SearchField extends React.Component {
 
@@ -9,7 +28,8 @@ class SearchField extends React.Component {
     super(props);
     this.state = {
        value: '',
-       timeplayed: null
+       timeplayed: null,
+       row_data: [{name: "Days", time_in_minutes:1440}, {name:"Hours", time_in_minutes:60}]
       }
 
     this.handleChange = this.handleChange.bind(this);
@@ -21,7 +41,12 @@ class SearchField extends React.Component {
   }
 
   keyPress(e) {
-    if (e.keyCode == 13) {
+    // 13 is Enter
+    if (e.keyCode === 13) {
+
+      this.setState({
+        row_data: this.state.row_data.concat(comparisonThings)
+      })
 
       const requestOptions = {
         method: 'POST',
@@ -30,7 +55,18 @@ class SearchField extends React.Component {
       };
 
       if (isNaN(e.target.value)) {
-        requestOptions.body = JSON.stringify({ vanity: e.target.value });
+        let pieces = e.target.value.split('/')
+        if (pieces.length == 0)
+        {
+          return
+        }
+
+        let vanityName = pieces[pieces.length - 1]
+        if (vanityName === "")
+        {
+          vanityName = pieces[pieces.length - 2]
+        }
+        requestOptions.body = JSON.stringify({ vanity: vanityName });
       }
 
       fetch('http://timeonsteam.com:2889/profileInfo', requestOptions)
@@ -40,17 +76,7 @@ class SearchField extends React.Component {
   }
 
   render() {
-    const classes = makeStyles((theme) => ({
-      App: {
-        display: 'flex',
-        flexWrap: 'wrap',
-      },
-      textField: {
-        marginLeft: theme.spacing(1),
-        marginRight: theme.spacing(1),
-        width: '25ch',
-      },
-    }));
+    const {classes} = this.props
     if (this.state.timeplayed == null)
     {
       return (
@@ -70,10 +96,33 @@ class SearchField extends React.Component {
     else
     {
       return (
-        <h2>You've played an insane {this.state.timeplayed} minutes on steam!</h2>
+        <Paper>
+          <h2>You've played an insane {this.state.timeplayed} minutes on steam!</h2>
+          <p>In that time you could've... </p>
+          <TableContainer component={Paper}>
+            <Table className={classes.table} size="small" aria-label="a dense table">
+            <TableHead>
+              <TableRow>
+                <TableCell>Done This</TableCell>
+                <TableCell align="right">This Many Times</TableCell>
+              </TableRow>
+            </TableHead>
+              <TableBody>
+                {this.state.row_data.map((row) => (
+                  <TableRow key={row.name}>
+                    <TableCell component="th" scope="row">
+                      {row.name}
+                    </TableCell>
+                    <TableCell align="right">{(this.state.timeplayed / row.time_in_minutes).toFixed(2)}</TableCell>
+                  </TableRow>
+                ))}
+              </TableBody>
+            </Table>
+          </TableContainer>
+        </Paper>
       )
     }
   }
 }
 
-export default SearchField;
+export default withStyles(useStyles)(SearchField);
